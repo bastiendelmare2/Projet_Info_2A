e436934ff42ad28a44957f8f8adf9b2e854d4fb4
enzo <enzo.crochet@eleve.ensai.fr>
2023-10-25 12:02:52 +0200

    test

--- METIER/Importation.py
+++ METIER/Importation.py
@@ -1,1 +1,17 @@
-print('bjr')
+import requests
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/import-data")
+async def import_data(url: str):
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+data = import_data(https://donnees.roulezeco.fr/opendata/instantane)
