bcec7e432d9045becfc33bb1750ac3783a8dca9d
Bastien Delmare <bastien.delmare2@gmail.com>
2023-10-26 16:05:25 +0200

    ui

--- METIER/StationsPreferees.py
+++ METIER/StationsPreferees.py
@@ -1,29 +1,29 @@
 # Import des classes
 from StationsServices import StationsServices
-from Utilisateur import Utilisateur
 from Horaires import Horaires
+from Coordonnees import Coordonnees
 from Services import Services
 
 class StationsPreferees:
-    def __init__(self, id_stations_pref, liste_stations, utilisateur):
+    def __init__(self, id_stations_pref, liste_stations):
         self.id_stations_pref = id_stations_pref
         self.liste_stations = liste_stations
-        self.utilisateur = utilisateur  # Associez directement l'utilisateur
 
     def __str__(self):
-        return f"Stations Préférées {self.id_stations_pref} de l'Utilisateur {self.utilisateur.nom_utilisateur}"
+        return f"Stations Préférées {self.id_stations_pref}, liste : {self.liste_stations}"
 
-# Exemple d'utilisation de la classe StationsPreferees avec des objets StationService et Utilisateur
-utilisateur = Utilisateur(123, "Nom de l'utilisateur", "prénom", 22)
-horaire1 = Horaires(1, ("Lundi, 10:00 - 12:00"))
-services1 = Services(4, 'laverie')
+# Exemple d'utilisation de la classe StationsPreferees avec des objets StationsServices
+horaire1 = Horaires()
+services1 = Services()
+coordonnees = Coordonnees("Ma Station", "48.8588443, 2.2943506",)
+
 
 liste_stations_preferees = [
     StationsServices(
-        1, "Ma Station", "48.8588443, 2.2943506", 54, [horaire1], [services1]
+        1,coordonnees, "Ma Station", "48.8588443, 2.2943506", 54, horaire1, [services1]
     )
 ]
 
-stations_preferees = StationsPreferees(1, liste_stations_preferees, utilisateur)
+stations_preferees = StationsPreferees(1, liste_stations_preferees)
 
 print(stations_preferees)
