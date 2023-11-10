be7827c3d07fcc88a4043aff6cf0c27f2c613968
Bastien Delmare <bastien.delmare2@gmail.com>
2023-11-10 12:37:39 +0100

    Hashage de mdp et reset table

---
+++ BDD/DAO_Reset_Tables.py
@@ -0,0 +1,25 @@
+from BDD.Connexion import DBConnection
+from utils.singleton import Singleton
+
+class SuppressionDonnees(metaclass=Singleton):
+    @staticmethod
+    def supprimer_donnees_tables():
+        tables = [
+            "projet2a.Stations_to_Services",
+            "projet2a.PrixCarburants",
+            "projet2a.Services",
+            "projet2a.Horaires",
+            "projet2a.Coordonnees",
+            "projet2a.StationsServices",
+            "projet2a.TypeCarburants"
+        ]
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    # D'abord, supprimer les données dans les tables qui n'ont pas de dépendances
+                    for table in tables:
+                        cursor.execute(f"DELETE FROM {table};")
+
+        except Exception as e:
+            print(e)
