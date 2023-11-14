b5179e30385116abb8fe9fcd1724b480482f2708
Bastien Delmare <bastien.delmare2@gmail.com>
2023-11-14 12:44:30 +0100

    Correction longitude et lattitude

--- main2.py
+++
@@ -1,4 +0,0 @@
-from BDD.DAO_StationsServices import StationsServices_Dao
-
-test = StationsServices_Dao.trouver_stations(StationsServices_Dao.filtre_stations(nom_type_carburant= "Gazole"), 25.2, 2.3, n= 5)
-print (test)
