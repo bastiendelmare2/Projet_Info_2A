2fa6a5f45e3f639a10d48d5b32b16c2a3de05bad
Bastien Delmare <bastien.delmare2@gmail.com>
2023-11-15 12:19:27 +0100

    petites modifs

--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
 from BDD.DAO_Reset_Tables import SuppressionDonnees
 from Alimtentation import Alimentation
-from METIER.Importation import XML
+from Importation import XML
 
 SuppressionDonnees.supprimer_donnees_tables()
 Alimentation.alimenter_toutes_tables(XML())
