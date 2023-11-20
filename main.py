7d6d99bb7151654a8e684b76bcff305951ddb245
Bastien Delmare <bastien.delmare2@gmail.com>
2023-11-20 16:57:22 +0100

    début API

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from METIER.ComptesUtilisateurs import ComptesUtilisateurs
-from BDD.DAO_Compte_Utilisateur import Compte_User_DAO
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+from starlette import status
+import uvicorn
+from SERVICES.Service_stations import Service_Station
+from BDD.DAO_StationsServices import StationsServices_Dao
 from SERVICES.Service_compte import ServiceCompte
 
-
-print(ServiceCompte.verifier_connexion(22,"quoi", "feur"))
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0")
